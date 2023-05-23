59a42d0fa958228174663b5131e559e8fb394179
Bryan <bryansanudo95@gmail.com>
2023-05-23 10:29:13 -0500

    view product

--- src/components/admin/Navbar.jsx
+++ src/components/admin/Navbar.jsx
@@ -8,25 +8,25 @@ const Navbar = () => {
       <ul className="menu bg-gray-300 h-full  ">
         <li>
           <Link to="home">
-            <FaFacebook className="text-4xl text-primary"/>
+            <FaFacebook className="text-4xl text-primary" />
             Inicio
           </Link>
         </li>
         <li>
           <Link to="all-products">
-            <FaFacebook className="text-4xl text-primary"/>
+            <FaFacebook className="text-4xl text-primary" />
             Productos
           </Link>
         </li>
         <li>
           <Link to="add-product">
-            <FaFacebook className="text-4xl text-primary"/>
+            <FaFacebook className="text-4xl text-primary" />
             Agregar
           </Link>
         </li>
         <li>
           <Link to="orders">
-            <FaFacebook className="text-4xl text-primary"/>
+            <FaFacebook className="text-4xl text-primary" />
             Pedidos
           </Link>
         </li>
--- src/components/admin/ViewProducts.jsx
+++ src/components/admin/ViewProducts.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { db } from "@/configFirebase";
 import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
+import { Link } from "react-router-dom";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 
 const ViewProducts = () => {
   const [products, setProducts] = useState([]);
@@ -32,7 +34,54 @@ const ViewProducts = () => {
     }
   };
 
-  return <div>ViewProducts</div>;
+  return (
+    <>
+      {products.length === 0 ? (
+        <p>No product found.</p>
+      ) : (
+        <table className="w-full md:mt-[90px] text-sm ">
+          <thead className="">
+            <tr className="h-20">
+              <th className="shadow-md shadow-primary rounded-xl">s/n</th>
+              <th className="shadow-md shadow-primary rounded-xl">Image</th>
+              <th className="shadow-md shadow-primary rounded-xl">Name</th>
+              <th className="shadow-md shadow-primary rounded-xl">Category</th>
+              <th className="shadow-md shadow-primary rounded-xl">Price</th>
+              <th className="shadow-md shadow-primary rounded-xl">Actions</th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {products.map((product, index) => {
+              const { id, name, price, imageURL, category } = product;
+              return (
+                <tr key={id} className="text-center  border-b-2">
+                  <td>{index + 1}</td>
+                  <td>
+                    <img
+                      src={imageURL}
+                      alt={name}
+                      className="h-[100px] w-full object-contain pt-4"
+                    />
+                  </td>
+                  <td className="text-center">{name}</td>
+                  <td className="text-center">{category}</td>
+                  <td className="text-center">{`$${price}`}</td>
+                  <td>
+                    {/*  <Link to="/admin/add-product">
+                    </Link> */}
+                    <div className="flex items-center justify-center gap-4">
+                      <FaEdit />
+                      <FaTrashAlt />
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 };
 
 export default ViewProducts;
