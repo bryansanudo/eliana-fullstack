a7bada993ec070707fbdb6da467d200ee33c23a5
Bryan <bryansanudo95@gmail.com>
2023-05-22 20:18:57 -0500

    add product and get all  products

--- src/components/admin/ViewProducts.jsx
+++ src/components/admin/ViewProducts.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { db } from "@/configFirebase";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
 
 const ViewProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
+    setIsLoading(true);
+
+    try {
+      const productsRef = collection(db, "products");
+      const q = query(productsRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(q, (snapshot) => {
+        const allProducts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(allProducts);
+        setProducts(allProducts);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
   return <div>ViewProducts</div>;
 };
 
