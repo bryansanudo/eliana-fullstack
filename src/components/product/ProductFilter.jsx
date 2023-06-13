6289296223bbaae42319bc71a13c567d2fc6aaf4
Bryan <bryansanudo95@gmail.com>
2023-06-13 17:09:32 -0500

    filter products by category

--- src/components/product/ProductFilter.jsx
+++ src/components/product/ProductFilter.jsx
@@ -1,14 +1,42 @@
-import { Link } from "react-router-dom";
-import { FaFacebook } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { selectProducts } from "@/redux/slice/productSlice";
+import { useState } from "react";
+import { FILTER_BY_CATEGORY } from "@/redux/slice/filterSlice";
 
 const ProductFilter = () => {
+  const [category, setCategory] = useState("All");
+  const products = useSelector(selectProducts);
+
+  const dispatch = useDispatch();
+
+  const allCategories = [
+    "All",
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  const filterProducts = (cat) => {
+    setCategory(cat);
+    dispatch(FILTER_BY_CATEGORY({ products, category: cat }));
+  };
+
   return (
     <>
       <h2 className="font-bold text-4xl py-8  text-transparent bg-clip-text  bg-gradient-to-r from-[#e67c04] to-[#5C24B3] w-min md:text-5xl capitalize">
         Categorias
       </h2>
-      <div>
-        <button>All</button>
+      <div className="flex flex-col items-start gap-2">
+        {allCategories.map((cat, index) => {
+          return (
+            <button
+              key={index}
+              type="button"
+              className={`${category}` === cat ? "" : null}
+              onClick={() => filterProducts(cat)}
+            >
+              {cat}
+            </button>
+          );
+        })}
       </div>
       <h4>Brand</h4>
       <div>
